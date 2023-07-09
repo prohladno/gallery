d77374ca9c76bb5979d296b8c2d7e4a1d2159953
prohladno <program.lera@gmail.com>
2023-07-09 16:29:57 +0200

    add simple logs

--- login.php
+++ login.php
@@ -7,6 +7,7 @@ $password = !empty($_POST['password']) ? $_POST['password'] : '';
 if (array_key_exists($login, $db)) {
     if($db[$login]['password'] == $password){
         setcookie('login', $_POST['login'], 0, '/');
+        writeLogin($login);
         header("Location: index.php");
     } else {
         echo 'Wrong password';
@@ -15,6 +16,14 @@ if (array_key_exists($login, $db)) {
 } else {
     echo 'User is not found';
     
+}
+
+
+function writeLogin($login)
+{
+    $logFile = fopen('log/log.txt', 'a');
+    fputs($logFile, 'login --- ' . $login . ' ' . date('Y-m-d H:i:s') . PHP_EOL);
+    fclose($logFile);
 }
 
 
--- logout.php
+++ logout.php
@@ -1,4 +1,15 @@
 <?php 
+
+function writeLogout($login)
+{
+    $logFile = fopen('log/log.txt', 'a');
+    fputs($logFile, 'logout --- ' . $login . ' ' . date('Y-m-d H:i:s') . PHP_EOL);
+    fclose($logFile);
+}
+
+$login = $_COOKIE['login'];
+writeLogout($login);
 setcookie('login', '', 0, '/');
+
 header('Location: index.php');
 ?>
